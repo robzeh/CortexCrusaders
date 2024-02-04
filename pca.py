f6e995eaf106d64b2e990d0d255ec667657414db
SciFi-Techno <145240512+SciFi-Techno@users.noreply.github.com>
2024-02-04 13:21:02 -0400

    Add files via upload

---
+++ pca.py
@@ -0,0 +1,18 @@
+### For testing only, did not provide the values as expected
+
+import torch
+import numpy as np
+from sklearn.decomposition import PCA
+
+DATA = "/Users/.../Projects/Dalhousie_MRI_Neural_Data_Hackathon/tensors"
+
+elr = torch.load(DATA + "/elr.pt") # (904, 368,176)
+elr_array = elr.numpy()
+
+elrStack = []
+for layer in range(0, 905):
+    pca = PCA(n_components=1)
+    components = pca.fit_transform(elr_array[layer])
+    elrStack.append(components)
+
+elrStack_array = np.array(elrStack)
