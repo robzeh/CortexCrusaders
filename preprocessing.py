0a35786fd6f7aa31b8d2253e32dc30dcd4f883fb
mayankr <mayank@dal.ca>
2024-02-04 11:42:29 -0400

    Found out top 5 brain areas for each task

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from scipy.signal import detrend
 from scipy.signal import savgol_filter
 from scipy.stats import ttest_ind
+from collections import Counter
 
 tasks = ["emotion", "motor", "language", "relational", "social", "gambling", "wm"]
 
@@ -20,6 +21,9 @@ def smooth(data, window_size=5):
     return smoothed_data
 
 mean_values_list = []
+
+# Dictionary to store indices of the top 10 values for each task
+top_10_indices_dict = {}
 
 for task in tasks:
     result_tensor = torch.load(f"/home/user002/projects/def-sponsor00/user002/tensors/{task}_result.pt")
@@ -46,11 +50,20 @@ for task in tasks:
     min_length = min(len(arr) for arr in mean_values_list)
     trimmed_mean_values = [arr[:min_length] for arr in mean_values_list]
 
+    sorted_indices = np.argsort(mean_values)[::-1]
+    top_10_indices = sorted_indices[:10]
+    top_10_indices_dict[task] = top_10_indices
+
+    flat_indices = [index for sublist in top_10_indices for index in sublist]
+    index_counter = Counter(flat_indices)
+    top_5_indices = index_counter.most_common(5)
+
     print(f"Task: {task}")
     print(f"Mean: {np.mean(mean_values)}")
     print(f"Standard Deviation: {np.std(mean_values)}")
     print(f"Min: {np.min(mean_values)}")
     print(f"Max: {np.max(mean_values)}")
+    print(f"Top 5 most common indices: {top_5_indices}")
     print()
     plt.plot(mean_values, label=task)
     plt.xlabel('Brain Areas')
@@ -63,9 +76,9 @@ for task in tasks:
 #    for j in range(i + 1, len(tasks)):
 #        task_1 = tasks[i]
 #        task_2 = tasks[j]
-#        min_length = min(len(trimmed_mean_values[task_1]), len(trimmed_mean_values[task_2]))
-#        t_statistic, p_value = ttest_ind(trimmed_mean_values[task_1][:min_length], 
-#                                          trimmed_mean_values[task_2][:min_length], 
+#        min_length = min(len(trimmed_mean_values[tasks.index(task_1)]), len(trimmed_mean_values[tasks.index(task_2)]))
+#        t_statistic, p_value = ttest_ind(trimmed_mean_values[tasks.index(task_1)][:min_length], 
+#                                          trimmed_mean_values[tasks.index(task_2)][:min_length], 
 #                                          alternative='two-sided', equal_var=False)
 #        
 #        print(f'Two-sample t-test for {task_1} vs {task_2}:')
